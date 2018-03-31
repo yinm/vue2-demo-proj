4750f7e56aad3b5b8737886c8ac8a12f5946ebf8
yinm <yinm.cminor9@gmail.com>
2018-03-31 17:52:39 +0900

    Refactor

--- src/GithubOutput/script.js
+++ src/GithubOutput/script.js
@@ -1,8 +1,12 @@
 import bus from '../bus'
 import Vue from 'vue'
+import GithubUserData from '../GithubUserData/index.vue'
 
 export default {
   name: 'GithubOutput',
+  components: {
+    'github-user-data': GithubUserData,
+  },
   created() {
     bus.$on('new-username', this.onUsernameChange)
   },
