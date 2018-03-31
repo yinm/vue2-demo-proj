5925d2c15074ee16b47e2425c3206c3539cc6e17
yinm <yinm.cminor9@gmail.com>
2018-03-31 17:08:45 +0900

    Add bus

--- src/GithubInput/script.js
+++ src/GithubInput/script.js
@@ -1,8 +1,11 @@
+import bus from '../bus'
+
 export default {
   name: 'GithubInput',
   methods: {
     onSubmit(event) {
       if (this.username && this.username !== '') {
+        bus.$emit('new-username', this.username)
       }
     }
   },
