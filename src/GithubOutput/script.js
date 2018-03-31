133164b65c1930ed45cb53aa177aaa62816313d1
yinm <yinm.cminor9@gmail.com>
2018-03-31 17:18:10 +0900

    Add new-username event

--- src/GithubOutput/script.js
+++ src/GithubOutput/script.js
@@ -2,6 +2,17 @@ import bus from '../bus'
 
 export default {
   name: 'GithubOutput',
+  created() {
+    bus.$on('new-username', this.onUsernameChange)
+  },
+  destroyed() {
+    bus.$off('new-username', this.onUsernameChange)
+  },
+  methods: {
+    onUsernameChange(name) {
+      this.currentUsername = name
+    }
+  },
   data() {
     return {
       currentUsername: null,
